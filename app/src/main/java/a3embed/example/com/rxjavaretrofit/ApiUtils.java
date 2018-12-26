a26c1fa667877b8aef471a284d5f1b66ab90ddbf
3Embed <pradyotprksh4@gmail.com>
2018-12-26 17:30:26 +0530

    Refact: Commit

--- app/src/main/java/a3embed/example/com/rxjavaretrofit/ApiUtils.java
+++ app/src/main/java/a3embed/example/com/rxjavaretrofit/ApiUtils.java
@@ -4,7 +4,7 @@ public class ApiUtils {
 
     private ApiUtils() {}
 
-    public static final String BASE_URL = "https://api.flexyapp.com/";
+    public static final String BASE_URL = "<<WRITE API HERE>>";
 
     public static RetroEndPoints getAPIService() {
         return RetrofitInstance.getClient(BASE_URL).create(RetroEndPoints.class);
